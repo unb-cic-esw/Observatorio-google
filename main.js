97bfbd06f0ad05e0ca2c47af3698277fb57493c7
CrimsonCrown <arthurscouto@outlook.com>
2018-03-29 08:26:03 -0300

    added text file to google from called listaat and changed code to google from that.

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 var google = require('google')
+var lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream(__dirname + '/' + 'listaat')//change path to a proper one later
+});
 
-google.resultsPerPage = 5
-var nextCounter = 0
+lineReader.on('line', function (line) {
 
-var pages = 1
-google('node.js best practices', function (err, res){
-  if (err) console.error(err)
+  google.resultsPerPage = 25
+  var nextCounter = 0
 
-  for (var i = 0; i < res.links.length; ++i) {
-    var link = res.links[i];
-    // console.log(link.title + ' - ' + link.href)
-    // console.log(link.description + "\n")
-    console.log(link.href)
-  }
+  google(line, function (err, res){
+    if (err) console.error(err)
 
-  if (nextCounter < pages-1) {
-    nextCounter += 1
-    if (res.next) res.next()
-  }
-})
+    for (var i = 0; i < res.links.length; ++i) {
+      var link = res.links[i];
+      console.log(link.href)
+      console.log(link.title)
+      console.log(link.description + "\n")
+    }
+
+    if (nextCounter < 4) {
+      nextCounter += 1
+      if (res.next) res.next()
+    }
+  })
+});
