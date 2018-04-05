2b74437a50ec7cbbb8ac87deb8fd33d712276b3f
leooleo <leo.ms097@gmail.com>
2018-04-05 10:05:31 -0300

    Adiciona padronização de arquivo saída e muda nome do scrape

---
+++ main_scrapper.js
@@ -0,0 +1,42 @@
+var google  = require('google')
+var fsmod   = require('fs')
+var persist = require('./persistence_module');
+
+var filename = 'listaat'
+if(fsmod.existsSync(__dirname + '/' + filename + '.txt')){
+  filename += '.txt'
+}
+else if(!(fsmod.existsSync(__dirname + '/' + filename))){
+  console.log('no input file/nao tem entrada')
+  return
+}
+var lineReader = require('readline').createInterface({
+  input: fsmod.createReadStream(__dirname + '/' + filename)//change path to a proper one later
+});
+
+lineReader.on('line', function (line) {
+
+  google.resultsPerPage = 25
+  var nextCounter = 0
+  //console.error(line);  
+  google(line, function (err, res){
+    // Escreve a palavra-chave    
+    persist.write(line + '\n');
+    if (err) {
+      console.error(err);
+      return
+    }
+
+
+    for (var i = 0; i < res.links.length; ++i) {
+      var link = res.links[i]
+      // Ignora links nulos
+      if (link.href == null) { continue; }
+      // Persiste os links no arquivo texto
+      persist.write('\t' + link.href + '\n');
+      //persist.write('\t' + link.title + '\n');
+      //persist.write('\t' + link.description + '\n');
+      console.log(link.href)      
+    }
+  })
+});
