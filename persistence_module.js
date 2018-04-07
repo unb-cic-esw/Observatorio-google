6ef9aa6753f630edcbf95b980cd2cd0acb6a4531
CrimsonCrown <arthurscouto@outlook.com>
2018-04-07 02:13:54 -0300

    now creates results folder automatically if it does not exist

--- persistence_module.js
+++ persistence_module.js
@@ -1,8 +1,17 @@
 module.exports = (fileName) => {
+	var dirName = __dirname + '/resultados';
 
 	var module = {};
 
 	var fs = require('fs');
+	if(!fs.existsSync(dirName)){
+    	fs.mkdirSync(dirName, 0766, function(err){
+        	if(err){
+            	console.log(err);
+            	response.send("diretorio nao pode ser criado.\n");
+        	}
+    	});
+	}
 
 	// O proposito deste modulo eh poder trocar a forma 
 	// de saida do programa sem alterar sua 'main'
