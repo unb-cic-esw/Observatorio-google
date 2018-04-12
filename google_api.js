614094ea3299ce1d46b8166d760192bfc5ed5718
leooleo <leo.ms097@gmail.com>
2018-04-12 09:01:21 -0300

    Melhora a robustez da api. Se a pesquisa conter invalidez, trata as exceções e retorna vazio. Por sua vez, a main detecta resultados vazios e não printa nada.

--- google_api.js
+++ google_api.js
@@ -12,9 +12,16 @@ function httpGet(theUrl){ //recupera os dados da query e retorna o JSON de resul
 
 exports.retrieveLinks = function(query, customsearchId, APIkey){ //monta a mensagem url da pesquisa a ser feita, extrai os dados que queremos do JSON resultante
 	var result = httpGet("https://www.googleapis.com/customsearch/v1?q=" + query + "&cx=" + customsearchId + "&key=" + APIkey + "&cr=countryBR")
-	result = JSON.parse(result)
+	result = JSON.parse(result)	
 	var resultLinks = []
-	for(var i = 0; i < 10; i++)
-		resultLinks.push(result["items"][i]["link"])
+	try{
+		for(var i = 0; i < 10; i++)		
+			resultLinks.push(result["items"][i]["link"])
+	}
+	catch(e) {
+		// Captura se o link retornado for nulo(Type Error)
+		resultLinks = []
+	}
+	// Caso tenha problema retorna apenas um vetor nulo
 	return resultLinks;
 }
--- main_api.js
+++ main_api.js
@@ -20,8 +20,12 @@ var lineReader = require('readline').createInterface({
 
 lineReader.on('line', function (line) { //para cara linha de listaat, uma busca no google
   var result = googleAPI.retrieveLinks(line, "005182128650059414634%3Acpbzek4imty", "AIzaSyC-6UytV9d7x16YvRzM1j-gdy1W3yTV-9w");
-  persist.write(line + '\n');
-  result.forEach((currentValue) => {
-  	persist.write('\t' + currentValue + '\n');
-  })
+  if(line && result) {
+    // Apenas escreve se linha e resultado são não-nulos
+    persist.write(line + '\n');
+    result.forEach((currentValue) => {
+      persist.write('\t' + currentValue + '\n');
+    })
+  }
+  
 });
