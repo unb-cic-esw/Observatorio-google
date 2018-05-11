2383434c64f2a99b9f86b4970fb11f6e6c84cea4
luisfbgs <arueluis@gmail.com>
2018-05-11 11:16:48 -0300

    Login in headless mode with an user-agent

--- utils/puppeteer_search.js
+++ utils/puppeteer_search.js
@@ -68,8 +68,8 @@ exports.googleSearch = async (page, query) => {
 */
 exports.newBrowser = async () => {
 	return await puppeteer.launch({
-		headless: false,
+		//headless: false,
 		// executablePath: '/usr/bin/google-chrome'
-		args: ['--no-sandbox', '--disable-setuid-sandbox']
+		args: ['--no-sandbox', '--disable-setuid-sandbox', '--user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36"']
 	});
 }
