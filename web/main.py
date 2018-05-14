8fc5d17ee66faf8b321955c06fdb6c7145741a05
luisfbgs <arueluis@gmail.com>
2018-05-13 22:24:22 -0300

    Web server debuged

--- web/main.py
+++ web/main.py
@@ -13,5 +13,6 @@ def actors():
 	with open('actors/actors.json') as data:
 		return jsonify(json.load(data))
 
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port, debug=True)
+if __name__ == '__main__':
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port, debug=True)
