499c8dbe0f4feeba70aeeeee2baa331ac6934658
luisfbgs <arueluis@gmail.com>
2018-05-14 22:35:44 -0300

    Adicionado o endpoint '/<date>/<actor>/all' para acessar todos os dados de um ator coletados em uma data

--- web/main.py
+++ web/main.py
@@ -13,5 +13,11 @@ def actors():
 	with open('actors/actors.json') as data:
 		return jsonify(json.load(data))
 
+@app.route('/<date>/<actor>/all', methods = ['GET'])
+def list_links(date, actor):
+	fileName = 'resultados/' + date + '/pup_' + actor + '/pup_' + actor + '.json'
+	with open(fileName) as data:
+		return jsonify(json.load(data))
+
 port = int(os.environ.get('PORT', 5000))
 app.run(host='0.0.0.0', port=port, debug=True)
