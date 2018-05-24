40001ee6bd6c3d8b0c277b13a3f92b7cd4fd315f
gtaumaturgo <gtaumaturg@gmail.com>
2018-05-24 10:54:30 -0300

    corrigido bug que fazia com que previsoes de resultados fossem puxadas de forma errada

--- utils/html_parse.py
+++ utils/html_parse.py
@@ -173,18 +173,28 @@ class ResultPreview():
 		self.data_flag = False
 		self.dados = []
 		self.dado = ""
+		self.spans_aninhados = 0
 
 	def shandletag(self, tagin, attrs):
 		if tagin == 'span' and ('class', 'st') in attrs:
-			self.data_flag = True
+			if self.spans_aninhados == 0:
+				self.spans_aninhados += 1
+				self.data_flag = True
+		elif tagin == 'span' and self.data_flag:
+			self.spans_aninhados += 1
+
 	def ehandletag(self, tagin):
 		if tagin == 'span' and self.data_flag:
-			self.dados.append(self.dado.strip())
-			self.dado = ''
-			self.data_flag = False
+			self.spans_aninhados -= 1
+			if self.spans_aninhados == 0:
+				self.dados.append(self.dado.strip())
+				self.dado = ''
+				self.data_flag = False
+
 
 	def shandledata(self, data):
 		if self.data_flag:
+			print("to aqui")
 			self.dado += data
 
 class ResultTitle() :
