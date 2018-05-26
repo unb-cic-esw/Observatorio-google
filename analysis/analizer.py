14db788423f87b66b774c8d45fe23fd18055451e
leooleo <leo.ms097@gmail.com>
2018-05-26 13:47:11 -0300

    Terminada a parte de análise de texto, que agora mostra as palavras mais frequentes e as mais relevantes. Diminuido acoplamento e dependência dos dataframes por parte do analiser.py. Agora este apenas recebe um array de dataframes onde o usuario escolhe quais serão mandados.

--- analysis/analizer.py
+++ analysis/analizer.py
@@ -1,6 +1,13 @@
+# TODO
+# Adicionar posicao no dataframe(e shared count)
+# Extender o get keywords para possivelmente o texto do html 
+# (mais interessante por conta do cálculo da relevância)
+# Buscar diretamente do heroku os Jsons a partir de duas datas
+
 import pandas as pd
 import json
 import unicodedata
+from text_analyzer import analyze
 
 def CreateDataFrame(file_name):    
     # Json contem uma lista de dictionarys(data_coleta->conteudo)
@@ -25,15 +32,24 @@ def CreateDataFrame(file_name):
     # Cria o DataFrame a partir do array de dicionarios
     return pd.DataFrame(array_dictionaries)
 
-def domain_count():
-    # Concatena dois data
-    df = CreateDataFrame('in.json')
-    all_dfs = pd.concat([df,CreateDataFrame('in2.json')])
+def domain_count(data_frame_array):    
+    # Concatena todos os data frames do array em um so
+    all_dfs = pd.concat( data_frame_array )    
+    # Agrupa por dominio e conta as aparicoes de cada link
     count_df = all_dfs.groupby('domain').count()['link']
     print count_df
 
-df = CreateDataFrame('in2.json')
-all_titles = ' '.join(p for p in df['title'])
-# Converte de unicode pra string
-all_titles = unicodedata.normalize('NFKD', all_titles).encode('ascii','ignore')
-print all_titles
+def get_keywords(data_frame_array,num_keywords):
+    documents = []
+    for df in data_frame_array:
+        string = ' '.join(p for p in df['title'])
+        # Converte de unicode pra string
+        string = unicodedata.normalize('NFKD', string).encode('ascii','ignore')
+        documents.append(string)
+    
+    analyze( documents, num_keywords )
+
+all_dfs = [CreateDataFrame('in.json'),CreateDataFrame('in2.json')]
+domain_count(all_dfs)
+print 
+get_keywords(all_dfs,10)
--- analysis/text_analyzer.py
+++ analysis/text_analyzer.py
@@ -5,21 +5,19 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 import math
 import operator
 
-data1 = "Lula tera pre-candidatura lancada no dia 9 de junho e PT aposta que ... Sob gritos de apoio e protestos, Gleisi le carta de Lula a prefeitos ... Lula - Pagina inicial | Facebook Lula | Lula PT lancara candidatura de Lula em ato no dia 9 de junho, em Belo ... Caso Lula consiga ser candidato e venca disputa presidencial, tera de ... Lula - ISTOE Independente Ministro do STF autoriza visita de deputados ao ex-presidente Lula ... Reitor de Aparecida se desculpa apos pedir que 'Nossa Senhora ..."
-data2 = "Luiz Inacio Lula da Silva - Wikipedia Luiz Inacio Lula da Silva Fast Facts - CNN - CNN.com Luiz Inacio Lula da Silva | World | the Guardian Million-Dollar Accusations Fly Against Brazil's Jailed Ex-President Lula Luiz Inacio Lula da Silva - the New York Times Brazil's Lula: 'Only death will take me off streets' - BBC News - BBC.com Lula Cafe Perry Anderson  Lula's Brazil  LRB 31 March 2011 Lula's legacy | The Economist Lula May Be in Jail, but Brazil's Occupy Movement Won't Let Hope Die ... Profile: Luiz Inacio Lula da Silva | Brazil News | Al Jazeera"
-documents = [data1,data2]
-
 def filter_words(document):
+    # Retira StopWords(artigos, adverbios ... dos textos)
     wordsFiltered = []
-    stopWords = set(stopwords.words('portuguese'))
-    stopWordse = set(stopwords.words('english'))
+    stopWords_pt = set(stopwords.words('portuguese'))
+    stopWords_eng = set(stopwords.words('english'))
     tokens = word_tokenize(document)
     for w in tokens:
-        if (w not in stopWords) and (w not in stopWordse) and (len(w) > 1) and (w != '...') and (w != '\'s'):
+        if (w not in stopWords_pt) and (w not in stopWords_eng) and (len(w) > 1) and (w != '...') and (w != '\'s'):
             wordsFiltered.append(w)
     
     return wordsFiltered
 def word_frequency(document):
+    # Num vezes aparece/Total palavras do doc
     word_freq = Counter(document)
     for i in word_freq:
         word_freq[i] = word_freq[i]/len(word_freq)
@@ -40,25 +38,43 @@ def inverse_word_frequency(documents,term_frequency):
         idf[term] = calc
     return idf
 
-for i,val in enumerate(documents):
-    documents[i] = filter_words(documents[i])
+def analyze(documents,num_palavras_chaves):    
+    for i,val in enumerate(documents):
+        # Fitra as palavras de todos os documentos
+        documents[i] = filter_words(documents[i].lower())
 
-idf = {}
-for doc in documents:    
-    tf = word_frequency(doc)      
-    idf = dict(idf, ** inverse_word_frequency(documents,tf))       
+    # Cria um dictionary contendo todos os termos e respectivos idfs
+    idf = {}
+    # Cria uma lista contendo todas as frequencias de termos
+    word_frequency_list = []
+    for doc in documents:    
+        tf = word_frequency(doc)    
+        word_frequency_list.append(tf)
+        # Usa o tf para calcular o idf      
+        idf = dict(idf, ** inverse_word_frequency(documents,tf))       
 
-first_tf = word_frequency(documents[0])
-second_tf = word_frequency(documents[1])
-relevance_list = {}
-for i in first_tf:
-    relevance_list[i] = first_tf[i]*idf[i]
+    # Cria uma lista de relevancia 
+    relevance_list = {}    
+    for term_index,val in enumerate(word_frequency_list):
+        term_frequency = word_frequency_list[term_index]
+        for i in term_frequency:
+            relevance_list[i] = term_frequency[i] * idf[i]
 
-for i in second_tf:
-    relevance_list[i] = second_tf[i]*idf[i]
+    # Cria uma lista de frequencia
+    top_frequency = Counter(documents[0])
+    for i in range(1,len(documents)):
+        y = Counter(documents[i])
+        top_frequency = { k: top_frequency.get(k,0) + y.get(k,0) for k in set(top_frequency) }
+        
+    # Ordena de forma descendente as listas de 'top'
+    top_relevant = sorted(relevance_list.items(), key=operator.itemgetter(1))
+    top_relevant = top_relevant[::-1]
 
-sorted_dict = sorted(relevance_list.items(), key=operator.itemgetter(1))
-sorted_dict = sorted_dict[::-1]
-for i in  sorted_dict[:10]:
-    print i
-    
+    top_frequency = sorted(top_frequency.items(), key=operator.itemgetter(1))
+    top_frequency = top_frequency[::-1]
+    # Mostra os 10 itens mais relevantes e frequentes daquela pesquisa
+    top_relevant = [x[0] for x in top_relevant[:num_palavras_chaves]]
+    top_frequency = [str(x[0]) + '(' + str(x[1]) + ')' for x in top_frequency[:num_palavras_chaves]]
+    print num_palavras_chaves, "palavras mais relevantes: " + ', '.join(top_relevant)
+    print
+    print num_palavras_chaves, "palavras mais frequentes: " + ', '.join(top_frequency)
