ece8f8eea0f48607036adbc0e4bdc8c1fcf6e7a8
leooleo <leo.ms097@gmail.com>
2018-06-04 14:55:45 -0300

    Adiciona integração do data science com o heroku.

--- analysis/analizer.py
+++ analysis/analizer.py
@@ -1,12 +1,21 @@
-# TODO
-# Buscar diretamente do heroku os Jsons a partir de duas datas
-
 import pandas as pd
 import json
 import unicodedata
+import os
+import urllib.request
 from text_analyzer import analyze
-from sharedcount import sharedcount
 
+
+def sharedcount(url):
+    requestUrl = 'http://api.sharedcount.com/?url=' + url + "&apikey=" + os.environ['SharedCountAPI']
+    
+    try:
+        apiData = urllib.request.urlopen(requestUrl)
+        apiJson = json.loads(apiData.read().decode('utf-8'))
+    except:
+        exit('Error')
+
+    return apiJson
 
 # DataFrame contém: hora_busca,link, dominio do link, titulo, 
 # posicao na busca, preview e shared count do link e dominio
@@ -15,21 +24,35 @@ def get_domain(link):
     right_domain = link[link.index('/')+2:]
     right_domain = right_domain[:right_domain.index('/')]
     return left_domain + right_domain
-def CreateDataFrame(file_name):    
+
+def getJson(actor):
+    # Pega os dados do heroku
+    print('Fetching '  + actor + ' data...')
+    with urllib.request.urlopen("https://observatorio-google.herokuapp.com/api/pesquisas/ator/" + actor) as url:
+        data = json.loads(url.read().decode())
+        return data
+    print('Done!')
+def get_json_info(list,name):
+    result = []
+    for dict in list:
+        result.append(dict[name])
+    return result        
+
+def CreateDataFrame(actor):
     # Json contem uma lista de dictionarys(data_coleta->conteudo)
     # Esta funcao cria um DataFrame contendo: data_coleta, link, dominio
+    print('Creating Dataframe...')
     array_dictionaries = []
-    file = json.loads(open(file_name).read())    
-    for array in file:
-        position = 0
-        time_search = array["horario"]
-        this_data   = array["dados"]
-        links       = this_data["Links de Resultados"]        
-        titles      = this_data["Titulos de Resultados"]
-        previews    = this_data["Previsoes de Resultados"]
+    for array in getJson(actor):
+        position = 0        
+        time_search = array['dados']['data']
+        this_data   = array['dados']['Resultado']
+        links       = get_json_info( this_data, 'linkResultados'     )
+        titles      = get_json_info( this_data, 'tituloResultados'   )        
+        previews    = get_json_info( this_data, 'previsaoResultados' )
         for link,title,preview in zip(links,titles,previews):
             position += 1
-        	# Extrai o dominio a partir de string split
+            # Extrai o dominio a partir de string split
             domain = get_domain(link)            
             # Pega o shared count(Facebook) do link
             data = sharedcount(link)
@@ -40,11 +63,12 @@ def CreateDataFrame(file_name):
             
             #Cria um dicionario para o array de dicionarios
             this_dictionary = {'time': time_search,'link': link,'domain': domain,
-                               'title': title,'position': position,'preview': preview,
-                               'link_sc': link_sc, 'domain_sc': domain_sc }
+                            'title': title,'position': position,'preview': preview,
+                            'link_sc': link_sc, 'domain_sc': domain_sc }
             array_dictionaries.append(this_dictionary)
 
     # Cria o DataFrame a partir do array de dicionarios
+    print('Created!')
     return pd.DataFrame(array_dictionaries)
 
 def domain_count(data_frame_array):    
@@ -64,11 +88,11 @@ def get_keywords(data_frame_array,num_keywords):
 
 # Colocar em all_dfs uma lista de todos os dataframes
 # Contenedo os Jsons dentre um range de datas
-all_dfs = [CreateDataFrame('in.json'),CreateDataFrame('in2.json')]
-#Count itera sobre o array de dataframes apenas para fins de print
+all_dfs = [CreateDataFrame('Lula')]
+# Count itera sobre o array de dataframes apenas para fins de print
 count = 1
 for dataframe in all_dfs:
-    #Busca uma correlação entre o shared count do dominio/link e a posição    
+    # Busca uma correlação entre o shared count do dominio/link e a posição    
     print("Correlação entre o shared count do link e posição no " +
         str(count) + "º dataframe: ", dataframe['link_sc'].corr(dataframe['position']))
     print("Correlação entre o shared count do dominio e posição no " + 
