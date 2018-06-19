3eea931b8bd2456cd5f8466d20d27bf01c5cf2ff
luisfbgs <arueluis@gmail.com>
2018-06-19 17:02:05 -0300

    Refatoração de roda_pesquisas.py

--- roda_pesquisas.py
+++ roda_pesquisas.py
@@ -1,26 +1,34 @@
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-import sys
-sys.path.insert(0, sys.path[0] + "/html_scanner")
-from html_scanner import extract_info
+""" Abre o navegador Firefox utilizando selenium e realiza uma lista de buscas
+    no google em uma série de contas do gmail e salva os resultados destas buscas
+    em um banco de dados """
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*
+
+import os
 import json
 import time
 import datetime
 import requests
-import os
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from html_scanner.html_scanner import extract_info
 
 def google_search(driver, query):
-    baseLink = "https://www.google.com/search?q="
+    """ Navega driver para a página com resultado da pesquisa no google do termo query """
 
-    driver.get(baseLink + query)
+    base_link = "https://www.google.com/search?q="
+
+    driver.get(base_link + query)
 
     return driver.page_source
 
 def gmail_sign_in(driver, profile):
+    """ Navega driver para a página do gmail e realiza o login na conta especificada por profile """
+
     driver.get("http://mail.google.com")
 
-    emailid = driver.find_element_by_id("identifierId")
-    emailid.send_keys(profile["login"])
+    email_id = driver.find_element_by_id("identifierId")
+    email_id.send_keys(profile["login"])
 
     nxt = driver.find_element_by_id("identifierNext")
     nxt.click()
@@ -35,29 +43,38 @@ def gmail_sign_in(driver, profile):
 
     time.sleep(5)
 
-if __name__ == "__main__":
-    firefoxProfile = webdriver.FirefoxProfile()
-    firefoxProfile.set_preference("general.useragent.override", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36")    
-    
+def main():
+    """ Abre o navegador Firefox utilizando selenium e realiza uma lista de buscas
+     no google em uma série de contas do gmail """
+
+    fireforx_profile = webdriver.FirefoxProfile()
+    fireforx_profile.set_preference(
+        "general.useragent.override",
+        """Mozilla/5.0 (Windows NT 6.1; Win64; x64)
+         AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36""")
+
     options = Options()
     options.set_headless(headless=True)
 
-    with open("profiles.json") as profilesFile: 
-        profiles = json.load(profilesFile)
+    with open("profiles.json") as profiles_file:
+        profiles = json.load(profiles_file)
 
         for profile in profiles:
-            driver = webdriver.Firefox(firefoxProfile, firefox_options=options)
-            
+            driver = webdriver.Firefox(fireforx_profile, firefox_options=options)
+
             if profile["login"] != "":
                 gmail_sign_in(driver, profile)
 
             with open("actors/actors.json") as actors:
                 buscas = json.load(actors)
                 for busca in buscas["atores"]:
-                    googleHtml = google_search(driver, busca)
-                    jsonData = extract_info(googleHtml)
-                    jsonData["data"] = str(datetime.date.today())
-                    jsonData["ator"] = busca
-                    jsonData["perfil"] = profile["login"]
-                    requests.post(os.environ["POST_URL"], json=jsonData)
+                    google_html = google_search(driver, busca)
+                    json_data = extract_info(google_html)
+                    json_data["data"] = str(datetime.date.today())
+                    json_data["ator"] = busca
+                    json_data["perfil"] = profile["login"]
+                    requests.post(os.environ["POST_URL"], json=json_data)
             driver.close()
+
+if __name__ == "__main__":
+    main()
